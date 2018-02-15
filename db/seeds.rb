180645b361c8bb7341c56d087bfef4794355b275
Wendi Li <wendicodes@gmail.com>
2018-02-15 14:29:22 +0100

    routes and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+
+array_of_hashes = ingredients['drinks']
+
+array_of_hashes.each do |hash|
+  name = hash["strIngredient1"]
+  Ingredient.create(name: name)
+end
